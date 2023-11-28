4ef1c27e6f7a30e2a2655290f703a2c724c1bb86
Kannan Ramamoorthy <kannanr@techconative.com>
2023-11-29 00:31:55 +0530

    Update merge_lora.py

--- scripts/merge_lora.py
+++ scripts/merge_lora.py
@@ -14,15 +14,15 @@ sys.path.append(str(wd))
 from lit_gpt.lora import GPT, Config, lora_filter, merge_lora_weights
 from lit_gpt.utils import check_valid_checkpoint_dir, get_default_supported_precision, lazy_load
 
-lora_r = 8
-lora_alpha = 16
+lora_r = 16
+lora_alpha = 32
 lora_dropout = 0.05
 lora_query = True
-lora_key = False
+lora_key = True
 lora_value = True
-lora_projection = False
-lora_mlp = False
-lora_head = False
+lora_projection = True
+lora_mlp = True
+lora_head = True
 
 
 def merge_lora(
